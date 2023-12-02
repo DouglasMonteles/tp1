ccd4ff79cb02cca28ce2e86353b93c8d06b788b4
douglasmonteles <silvadouglas328@gmail.com>
2023-12-01 23:38:56 -0300

    Adding structure about exceptions tests

---
+++ src/test/java/com/fga/tppe/tp1/TestesExcecoes.java
@@ -0,0 +1,12 @@
+package com.fga.tppe.tp1;
+
+import org.junit.experimental.categories.Categories;
+import org.junit.experimental.categories.Categories.IncludeCategory;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Categories.class)
+@SuiteClasses({})
+@IncludeCategory(TestesExcecoes.class)
+public class TestesExcecoes {
+}
