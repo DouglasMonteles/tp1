5653079f6582bb69ffa3f4fc5b83acfcb95c4390
ingridSCarvalho <69825746+ingridSCarvalho@users.noreply.github.com>
2023-12-03 18:49:44 -0300

    Parametrização: Teste coerencia da quantidade de produto

--- src/test/java/com/fga/tppe/tp1/TestesFuncionais.java
+++ src/test/java/com/fga/tppe/tp1/TestesFuncionais.java
@@ -1,13 +1,14 @@
 package com.fga.tppe.tp1;
 
 import com.fga.tppe.tp1.models.AlertaEstoqueBaixoTeste;
+import com.fga.tppe.tp1.models.TestCoerenciaQuantidade;
 import org.junit.experimental.categories.Categories;
 import org.junit.experimental.categories.Categories.IncludeCategory;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Categories.class)
-@SuiteClasses({ AlertaEstoqueBaixoTeste.class })
+@SuiteClasses({ AlertaEstoqueBaixoTeste.class, TestCoerenciaQuantidade.class})
 @IncludeCategory(TestesFuncionais.class)
 public class TestesFuncionais {
 }
--- src/test/java/com/fga/tppe/tp1/models/TestCoerenciaQuantidade.java
+++ src/test/java/com/fga/tppe/tp1/models/TestCoerenciaQuantidade.java
@@ -4,51 +4,56 @@ import com.fga.tppe.tp1.TestesFuncionais;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
 
 @Category(TestesFuncionais.class)
+@RunWith(Parameterized.class)
 public class TestCoerenciaQuantidade {
 
             private ItemTransacao itemTransacao1;
-            private ItemTransacao itemTransacao2;
-            private ItemTransacao itemTransacao3;
             private Produto produto1;
-            private Produto produto2;
-            private Produto produto3;
+
+            private Integer quantidadeDisponivel;
+            private Integer quantidade;
+
+            private Boolean resultadoEsperado;
 
             @Before
             public void setup() {
                 this.produto1 = new Produto();
-                this.produto2 = new Produto();
-                this.produto3 = new Produto();
-                this.produto1.setQuantidadeDisponivel(9);
-                this.produto2.setQuantidadeDisponivel(6);
-                this.produto3.setQuantidadeDisponivel(3);
+                this.produto1.setQuantidadeDisponivel(quantidadeDisponivel);
                 this.itemTransacao1 = new ItemTransacao();
-                this.itemTransacao2 = new ItemTransacao();
-                this.itemTransacao3 = new ItemTransacao();
-                this.itemTransacao1.setQuantidade(3);
-                this.itemTransacao2.setQuantidade(6);
-                this.itemTransacao3.setQuantidade(9);
+                this.itemTransacao1.setQuantidade(quantidade);
                 this.itemTransacao1.setProduto(produto1);
-                this.itemTransacao2.setProduto(produto2);
-                this.itemTransacao3.setProduto(produto3);
             }
 
-            @Test
-            public void naoEmiteAlertaQuandoItemTransacaoMenorQueQuantidadeDisponivel() {
-                assertFalse(this.itemTransacao1.alertaQuantidade());
+            public TestCoerenciaQuantidade(Integer quantidadeDisponivel, Integer quantidade, Boolean resultadoEsperado){
+                this.quantidadeDisponivel = quantidadeDisponivel;
+                this.quantidade = quantidade;
+                this.resultadoEsperado = resultadoEsperado;
             }
 
-            @Test
-            public void emiteAlertaQuandoItemTransacaoIgualAQuantidadeDisponivel() {
-                assertTrue(this.itemTransacao2.alertaQuantidade());
-            }
+        @Parameterized.Parameters
+        public static Collection<Object[]> getParameters() {
+            Object[][] parametros = new Object[][] {
+                    // quantidadeDisponivel, quantidade
+                    {9, 3, false},
+                    {6, 6, true},
+                    {3, 9, true},
+            };
 
-            @Test
-            public void emiteAlertaQuandoItemQuantidadeMaiorQueQuantidadeDisponivel() {
-                assertTrue(this.itemTransacao3.alertaQuantidade());
+            return Arrays.asList(parametros);
+        }
+
+
+    @Test
+            public void alertaQuantidadeItemTransacaoQuantidadeDisponivel() {
+                assertEquals(resultadoEsperado, this.itemTransacao1.alertaQuantidade());
             }
         }
