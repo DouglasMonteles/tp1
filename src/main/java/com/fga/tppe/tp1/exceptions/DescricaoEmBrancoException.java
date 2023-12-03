d78d16d5df404ffbbcf11acfa15572026734801e
Giulia Alcantara <alcantaragiubs@gmail.com>
2023-12-03 18:24:09 -0300

    Cadastro de Protudo - TDD

---
+++ src/main/java/com/fga/tppe/tp1/exceptions/DescricaoEmBrancoException.java
@@ -0,0 +1,13 @@
+package com.fga.tppe.tp1.exceptions;
+
+public class DescricaoEmBrancoException extends RuntimeException {
+
+    public DescricaoEmBrancoException() {
+        super();
+    }
+
+    public DescricaoEmBrancoException(String msg) {
+        super(msg);
+    }
+
+}
--- src/test/java/com/fga/tppe/tp1/TestesExcecoes.java
+++ src/test/java/com/fga/tppe/tp1/TestesExcecoes.java
@@ -1,7 +1,7 @@
 package com.fga.tppe.tp1;
 
 import com.fga.tppe.tp1.models.AlertaEstoqueBaixoTesteExcecao;
-
+import com.fga.tppe.tp1.models.CadastroProdutoExcecao;
 import com.fga.tppe.tp1.models.TestesGestaoTransacoesExcecao;
 import org.junit.experimental.categories.Categories;
 import org.junit.experimental.categories.Categories.IncludeCategory;
@@ -11,7 +11,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Categories.class)
 @SuiteClasses({
         AlertaEstoqueBaixoTesteExcecao.class,
-        TestesGestaoTransacoesExcecao.class
+        TestesGestaoTransacoesExcecao.class,
+        CadastroProdutoExcecao.class
 })
 @IncludeCategory(TestesExcecoes.class)
 public class TestesExcecoes {
