b2437a688b6509e73eabfcd7894361ee142279a0
ingridSCarvalho <69825746+ingridSCarvalho@users.noreply.github.com>
2023-12-04 00:00:29 -0300

    Parametrização: Teste conferência da data de validade do lote

--- src/test/java/com/fga/tppe/tp1/TestesFuncionais.java
+++ src/test/java/com/fga/tppe/tp1/TestesFuncionais.java
@@ -2,6 +2,7 @@ package com.fga.tppe.tp1;
 
 import com.fga.tppe.tp1.models.AlertaEstoqueBaixoTeste;
 import com.fga.tppe.tp1.models.TestCoerenciaQuantidade;
+import com.fga.tppe.tp1.models.TestDataValidade;
 import com.fga.tppe.tp1.models.TestTotalItensLote;
 import org.junit.experimental.categories.Categories;
 import org.junit.experimental.categories.Categories.IncludeCategory;
@@ -9,7 +10,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Categories.class)
-@SuiteClasses({ AlertaEstoqueBaixoTeste.class, TestCoerenciaQuantidade.class, TestTotalItensLote.class})
+@SuiteClasses({ AlertaEstoqueBaixoTeste.class, TestCoerenciaQuantidade.class, TestTotalItensLote.class, TestDataValidade.class})
 @IncludeCategory(TestesFuncionais.class)
 public class TestesFuncionais {
 }
--- src/test/java/com/fga/tppe/tp1/models/TestDataValidade.java
+++ src/test/java/com/fga/tppe/tp1/models/TestDataValidade.java
@@ -1,41 +1,57 @@
 package com.fga.tppe.tp1.models;
 
+import com.fga.tppe.tp1.TestesFuncionais;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+@Category(TestesFuncionais.class)
+@RunWith(Parameterized.class)
 public class TestDataValidade {
 
     private Lote lote;
     private Produto produto;
+    private Boolean resultado;
+    private Integer id;
+    private Date validade;
+    private Double precoVenda;
 
     @Before
     public void setup() {
         this.lote = new Lote();
-        this.lote.setId(1);
-        this.lote.setValidade(new Date(1234567890000L));
+        this.lote.setId(id);
+        this.lote.setValidade(validade);
         this.produto = new Produto();
-        this.produto.setPrecoVenda(10.00);
+        this.produto.setPrecoVenda(precoVenda);
         this.produto.setLote(lote);
     }
 
-    @Test
-    public void AlertaConfereValidadeDentroPrazo() {
-        assertTrue(this.produto.confereValidade());
+    public TestDataValidade(Integer id, Date validade , Double precoVenda, Boolean resultado){
+        this.id = id;
+        this.validade = validade;
+        this.precoVenda = precoVenda;
+        this.resultado = resultado;
+    }
+    @Parameterized.Parameters
+    public static Collection<Object[]> getParameters() {
+        Object[][] parametros = new Object[][] {
+                // lote, validade, precoVenda
+                {1, new Date(9999999990000L), 10.00, false},
+        };
+
+        return Arrays.asList(parametros);
     }
     @Test
-    public void AlertaConfereValidadeForaPrazo() {
-        this.lote = new Lote();
-        this.lote.setId(2);
-        this.lote.setValidade(new Date(9999999990000L));
-        this.produto = new Produto();
-        this.produto.setPrecoVenda(10.00);
-        this.produto.setLote(lote);
-        assertFalse(this.produto.confereValidade());
+    public void AlertaConfereValidadeDentroPrazo() {
+        assertEquals(resultado, this.produto.confereValidade());
     }
 }
