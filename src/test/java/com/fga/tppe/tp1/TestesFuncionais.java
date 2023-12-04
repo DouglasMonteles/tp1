40f1bd58b8f78764c0d33947a2fa5e2be916ace7
douglasmonteles <silvadouglas328@gmail.com>
2023-12-03 21:18:32 -0300

    Parametrizacao: Teste de transferencia entre filiais

--- src/test/java/com/fga/tppe/tp1/TestesFuncionais.java
+++ src/test/java/com/fga/tppe/tp1/TestesFuncionais.java
@@ -1,6 +1,7 @@
 package com.fga.tppe.tp1;
 
 import com.fga.tppe.tp1.models.AlertaEstoqueBaixoTeste;
+import com.fga.tppe.tp1.models.TesteTransferenciaEntreFiliais;
 import com.fga.tppe.tp1.models.TestesGestaoTransacoes;
 import org.junit.experimental.categories.Categories;
 import org.junit.experimental.categories.Categories.IncludeCategory;
@@ -8,7 +9,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Categories.class)
-@SuiteClasses({ AlertaEstoqueBaixoTeste.class, TestesGestaoTransacoes.class})
+@SuiteClasses({
+        AlertaEstoqueBaixoTeste.class,
+        TestesGestaoTransacoes.class,
+        TesteTransferenciaEntreFiliais.class
+})
 @IncludeCategory(TestesFuncionais.class)
 public class TestesFuncionais {
 }
--- src/test/java/com/fga/tppe/tp1/models/TesteTransferenciaEntreFiliais.java
+++ src/test/java/com/fga/tppe/tp1/models/TesteTransferenciaEntreFiliais.java
@@ -1,15 +1,36 @@
 package com.fga.tppe.tp1.models;
 
+import com.fga.tppe.tp1.TestesFuncionais;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Category(TestesFuncionais.class)
+@RunWith(Parameterized.class)
 public class TesteTransferenciaEntreFiliais {
 
     private Transferencia transferencia;
     private ItemTransacao itemTransacao;
     private Produto produto;
+    private String origem;
+    private String destino;
+    private Boolean resultadoEsperado;
+
+    public TesteTransferenciaEntreFiliais(String origem,
+                                          String destino,
+                                          Boolean resultadoEsperado) {
+        this.origem = origem;
+        this.destino = destino;
+        this.resultadoEsperado = resultadoEsperado;
+    }
 
     @Before
     public void setup() {
@@ -17,27 +38,29 @@ public class TesteTransferenciaEntreFiliais {
         this.itemTransacao = new ItemTransacao();
         this.produto = new Produto();
 
-        produto.setLocalizacao("São Paulo");
+        produto.setLocalizacao(origem);
         itemTransacao.setProduto(produto);
         transferencia.addItem(itemTransacao);
+
+        this.transferencia.setOrigem(origem);
+        this.transferencia.setDestino(destino);
+    }
+
+    @Parameters
+    public static Collection<Object[]> getParameters() {
+        Object[][] parametros = new Object[][] {
+                // origem, destino, resultadoEsperado
+                {"São Paulo", "Rio de Janeiro", true},
+                {"São Paulo", "São Paulo",      false},
+        };
+
+        return Arrays.asList(parametros);
     }
 
     @Test
     public void testTranferenciaFiliais() {
-        this.transferencia.setOrigem(produto.getLocalizacao());
-        this.transferencia.setDestino("Rio de Janeiro");
-
-        assertTrue(this.transferencia.registrarTransferencia());
-        assertEquals("Rio de Janeiro", produto.getLocalizacao());
-    }
-
-    @Test
-    public void testnaoTranfereEntreFiliaisQuandoOrigemEoDestinoSaoIguais() {
-        this.transferencia.setOrigem(produto.getLocalizacao());
-        this.transferencia.setDestino(produto.getLocalizacao());
-
-        assertFalse(this.transferencia.registrarTransferencia());
-        assertEquals("São Paulo", produto.getLocalizacao());
+        assertEquals(resultadoEsperado, this.transferencia.registrarTransferencia());
+        assertEquals(destino, produto.getLocalizacao());
     }
 
 }
